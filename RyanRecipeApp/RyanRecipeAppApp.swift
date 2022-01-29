6efa92db01a3924ddd75a0c0793c05fb5062ab8a
Ryan Callaghan <RyanCallaghan2020@u.northwestern.edu>
2022-01-29 15:02:21 -0600

    Initial Commit

---
+++ RyanRecipeApp/RyanRecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RyanRecipeAppApp.swift
+//  RyanRecipeApp
+//
+//  Created by Ryan Callaghan on 1/22/22.
+//
+
+import SwiftUI
+
+@main
+struct RyanRecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RecipeListView()
+        }
+    }
+}
